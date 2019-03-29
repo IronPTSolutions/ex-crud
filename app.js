f02b6c0710b9db13f41ada4c40cd1394e5e76307
Carlos del Prado <carlos.delprado@11paths.com>
2019-03-29 21:05:31 +0100

    feat: navbar

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => res.redirect('/books'));
 app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
